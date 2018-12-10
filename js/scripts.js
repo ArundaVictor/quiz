6216f7fcfd2dd6776365e76e1d76fa8b251cd559
ArundaVictor <victoryugi31@gmail.com>
2018-12-10 21:19:20 +0300

    make changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
   $(document).ready(function() {
-    var answers = []
-      answers.push(ans1, ans2, ans3, ans4, ans5);
-    var ans1 = parseInt($("input[name=q1]:checked").val());
-    var ans2 = parseInt($("input[name=q2]:checked").val());
-    var ans3 = parseInt($("input[name=q3]:checked").val());
-    var ans4 = parseInt($("input[name=q4]:checked").val());
-    var ans5 = parseInt($("input[name=q5]:checked").val());
+    function marks() {
+      var ans1 = document.querySelector('input[name="q1"]:checked').value;
+      var ans2 = document.querySelector('input[name="q2"]:checked').value;
+      var ans3 = document.querySelector('input[name="q3"]:checked').value;
+      var ans4 = document.querySelector('input[name="q4"]:checked').value;
+      var ans5 = document.querySelector('input[name="q5"]:checked').value;
+    var answer = 0;
+    if (ans1 === "script") {
+      answer += 10;
+  }
+  if (ans2 === "?") {
+      answer += 10;
+  }
+  if (ans3 === "var") {
+      answer += 10;
+  }
+  if (ans5 === "c2") {
+      answer += 10;
+  }
+  if (ans5 === "java") {
+      answer += 10;
+  }
+
+}
+
+
 
     $("#button").click(function result(form) {
-      for (i=0; i<=answers.length; i++) {
-        var total = 0;
-        total  += answers[i] <<0 ;
-        alert(total);
 
-    };
+
+
+        alert(answer);
+
+
 
 });
 $(".button").click(function() {
