9318b1ef5521dc99955f404d6d037e51c91e0f36
ArundaVictor <victoryugi31@gmail.com>
2018-12-10 17:38:07 +0300

    make new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
   $(document).ready(function() {
     var answers = []
+      answers.push(ans1, ans2, ans3, ans4, ans5);
     var ans1 = parseInt($("input[name=q1]:checked").val());
     var ans2 = parseInt($("input[name=q2]:checked").val());
     var ans3 = parseInt($("input[name=q3]:checked").val());
     var ans4 = parseInt($("input[name=q4]:checked").val());
     var ans5 = parseInt($("input[name=q5]:checked").val());
-    answers.push(ans1, ans2, ans3, ans4, ans5);
+
     $("#button").click(function result(form) {
       for (i=0; i<=answers.length; i++) {
         var total = 0;
-        total  += answers[i];
-        alert("Your score is /5"+answers)
+        total  += answers[i] <<0 ;
+        alert(total);
+
     };
 
 });
