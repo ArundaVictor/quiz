df6b83e9f93f64c90d129c0e841df45b8f6196b2
ArundaVictor <victoryugi31@gmail.com>
2018-12-11 10:32:20 +0300

    make new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
-  $(document).ready(function() {
-    var answers = []
-      answers.push(ans1, ans2, ans3, ans4, ans5);
-    var ans1 = parseInt($("input[name=q1]:checked").val());
-    var ans2 = parseInt($("input[name=q2]:checked").val());
-    var ans3 = parseInt($("input[name=q3]:checked").val());
-    var ans4 = parseInt($("input[name=q4]:checked").val());
-    var ans5 = parseInt($("input[name=q5]:checked").val());
+$(document).ready(function () {
+  $(".button").click(function() {
+    $("#questions").slideToggle();
+  });
+$("#button").click(function() {
+    var ans1 =parseInt($('input[name="q1"]:checked').val());
+    var ans2 =parseInt($('input[name="q2"]:checked').val());
+    var ans3 = parseInt($('input[name="q3"]:checked').val());
+    var ans4 = parseInt($('input[name="q4"]:checked').val());
+    var ans5 = parseInt($('input[name="q5"]:checked').val());
+    var score = 0;
+    if (ans1 == 10) {
+      score += 10;
+    }
+    if (ans2 ==10) {
+      score += 10;
+    }
+    if (ans3 == 10) {
+      score += 10;
+    }
+    if (ans4 ==10) {
+      score += 10;
+    }
+    if (ans5 == 10) {
+      score += 10;
+    }
+$("#result").val(score);
+$("#button").click(function () {
+  $("#questions").hide();
+  $("#result").slideToggle();
+})
 
-    $("#button").click(function result(form) {
-      for (i=0; i<=answers.length; i++) {
-        var total = 0;
-        total  += answers[i] <<0 ;
-        alert(total);
-
-    };
-
-});
-$(".button").click(function() {
-  $("#questions").slideToggle();
 });
 });
